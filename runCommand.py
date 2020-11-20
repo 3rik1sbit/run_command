04420b9d41cfdb179772a59efe02f7bf5fa4575b
Erik Ivarsson <erikoivarsson@gmail.com>
2020-11-20 21:48:04 +0100

    Use full path to commands

--- runCommand.py
+++ runCommand.py
@@ -1,10 +1,11 @@
 import os
 commands = []
-f = open("commands", "r")
+commands_path = "/home/erik/Projects/python/run_command/commands"
+f = open(commands_path, "r")
 for line in f:
     commands.append(line)
 f.close()
-f = open("commands", "w")
+f = open(commands_path, "w")
 f.write("")
 for command in commands:
     os.system(command)
